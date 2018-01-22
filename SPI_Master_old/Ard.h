63671b62d4c6b5fd990872ace2d732d0bccc1af4
Jay Garcia <jesusdgarcia@gmail.com>
2018-01-22 09:26:58 -0500

    Removed items not needed

--- SPI_Master_old/Ard.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARD_H
-#define ARD_H
-
-
-void ard_setup();
-void ard_step();
-
-
-#endif
--- SPI_Slave_DUE_RGB_Matrix/SPI_DMA.h
+++
@@ -1,4 +0,0 @@
-#include <Arduino.h>
-void setupSlave();
-
-uint8_t spiSendReceive(uint8_t* outBuf, uint8_t* inBuf, size_t len);
